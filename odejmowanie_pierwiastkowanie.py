d38cdc712e0c8901d694b0b9c9844222139b2f73
hdauharukava <hdauharukava@st.swps.edu.pl>
2023-12-14 23:11:33 +0100

    refactored code

--- odejmowanie_pierwiastkowanie.py
+++ odejmowanie_pierwiastkowanie.py
@@ -1,7 +1,16 @@
 import math
-def odejmowanie(a, b):
-    return a-b
-def pierwiastkowanie(a)
+
+
+def substraction(a, b):
+    return a - b
+
+
+def root(a):
     return math.sqrt(a)
 
 
+def division(a, b):
+    if b != 0:
+        return round(a / b, 3)
+    else:
+        return f"You can't divide by zero!"
